c769a0a7a7f0346cd1019605613026c16952cc91
Kim Holland <tripleaxis@gmail.com>
2016-12-28 14:45:41 +1100

    Updated data-structure and added common data query function

--- src/core/firebase/index.js
+++ src/core/firebase/index.js
@@ -1,7 +1,20 @@
 import firebase from 'firebase';
 import config from './config';
 
-export const app = firebase.initializeApp(config);
+firebase.initializeApp(config);
+
 export const database = firebase.database();
 export const auth = firebase.auth();
 export const GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
+
+export const query = (path) => {
+	return new Promise((resolve, reject) => {
+		database.ref(path)
+			.once('value', (response) => {
+				resolve(response.val());
+			})
+			.catch((err) => {
+				throw err.message;
+			});
+	});
+};
--- src/core/items/index.js
+++ src/core/items/index.js
@@ -1,4 +1,4 @@
-import { database } from '../firebase';
+import { query } from '../firebase';
 import store from '../store';
 import * as ItemActions from './actions';
 import * as ActionTypes from './action-types';
@@ -8,13 +8,9 @@ export { ActionTypes };
 
 // initial data request for items
 export function fetchItems (dispatch) {
-	return new Promise((resolve, reject) => {
-		database.ref('items')
-			.once('value', function (response) {
-				let items = response.val();
-				console.log('fetchItems::resolved', items);
-				store.dispatch(ItemActions.initItems(items));
-				resolve();
-			});
-	});
+	return query('artwork')
+		.then((artwork) => {
+			console.log('fetchItems::resolved', artwork);
+			store.dispatch(ItemActions.initItems(artwork.items));
+		});
 }
