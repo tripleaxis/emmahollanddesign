d3f9c39c0fb58fe499ad00db0ad9838142ce40e6
Kim Holland <tripleaxis@gmail.com>
2019-08-07 18:04:15 +1000

    chore: unit test fixes

--- test/specs/views/home.spec.js
+++ test/specs/views/home.spec.js
@@ -3,20 +3,20 @@ import View from '../../../src/views/home';
 import { shallow } from 'enzyme';
 
 describe('Home View', () => {
-  
-  let wrapper;
-  const store = {
-    getState: () => [{}]
+  const createComponent = () => {
+    const props = {
+      store: {
+        getState: () => [{}]
+      }
+    };
+
+    return shallow(<View {...props} />);
   };
-  
-  beforeEach(() => {
-    wrapper = shallow(<View store={store}/>);
+
+  it('should render without error', () => {
+    expect(createComponent).not.toThrow();
   });
-  
-  it('should exist', () => {
-    expect(View).toBeDefined();
-  });
-  
+
   it('should render a title', () => {
     expect(1).toBe(1);
   });
